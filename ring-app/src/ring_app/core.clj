69ad84ce5bb518b64a1617737215edc0a3ccb059
Park Hyeonjun <phjosh06@gmail.com>
2022-01-24 23:08:51 +0900

    Route Requests with Ring -- Adding Functionality with Middleware

--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,15 +1,24 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]))
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler [request-map]
   (response/response
-   (str "<html><body> your IP is : "
+   (str "<html><body>Hello, your IP is : "
         (:remote-addr request-map)
         "</body></html>")))
 
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+  
 (defn -main []
   (jetty/run-jetty
-   handler
+   (-> #'handler
+       wrap-nocache
+       wrap-reload)
    {:port 3000
     :join? false}))
