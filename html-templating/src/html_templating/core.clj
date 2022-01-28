e5354631947607fff0a45bdbf0b98fbbdf78260f
Hyeonjun Park <lazyskulptor@gmail.com>
2022-01-28 20:08:14 +0900

    HTML Templating Using Selmer -- Creating Templates

--- html-templating/src/html_templating/core.clj
+++ html-templating/src/html_templating/core.clj
@@ -4,3 +4,13 @@
 
 (defn fn-1 []
   (selmer/render "Hello, {{name}}" {:name "World"}))
+
+(defn fn-2-1 []
+  (selmer/render-file "hello.html" {:name "World"}))
+
+(defn fn-2-2 []
+  (selmer/render-file "hello-2.html" {:items (range 10)}))
+
+(defn fn-2-3 []
+  (selmer/render "<p>Hello {{user.first}} {{user.last}}</p>"
+                 {:user {:first "John" :last "Doe"}}))
--- html-templating/test/html_templating/core_test.clj
+++ html-templating/test/html_templating/core_test.clj
@@ -5,3 +5,18 @@
 (deftest a-test
   (testing "First REPL"
     (is (= "Hello, World" (fn-1)))))
+
+
+(deftest create-template
+  (testing "Using HTML File"
+    (is (= "<html>\n  <head>\n    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n    <title>My First Template</title>\n  </head>\n  <body>\n    <h2>Hello World</h2>\n  </body>\n</html>\n"
+           (fn-2-1)))))
+
+(deftest use-list
+  (testing "Using HTML File with list"
+    (is (= "<ul>\n  \n  <li> 0 </li>\n  \n  <li> 1 </li>\n  \n  <li> 2 </li>\n  \n  <li> 3 </li>\n  \n  <li> 4 </li>\n  \n  <li> 5 </li>\n  \n  <li> 6 </li>\n  \n  <li> 7 </li>\n  \n  <li> 8 </li>\n  \n  <li> 9 </li>\n  \n</ul>\n"
+           (fn-2-2)))))
+
+(deftest use-map
+  (testing "Using HTML File with map"
+    (is (= "<p>Hello John Doe</p>" (fn-2-3)))))
