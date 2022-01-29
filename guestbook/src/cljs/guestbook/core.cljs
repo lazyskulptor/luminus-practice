341351a74ca12c6332194a2aa61a9450a504a7d7
Hyeonjun Park <lazyskulptor@gmail.com>
2022-01-29 19:17:56 +0900

    Build the UI with Reagent -- Reimplementing the Form

--- guestbook/src/cljs/guestbook/core.cljs
+++ guestbook/src/cljs/guestbook/core.cljs
@@ -2,8 +2,36 @@
   (:require [reagent.core :as r]
             [reagent.dom :as dom]))
 
+(defn message-form []
+  (let [fields (r/atom {})]
+    (fn []
+      [:div
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [:input.input
+         {:type :text
+          :name :name
+          :on-change #(swap! fields
+                             assoc :name (-> % .-target .-value))
+          :value (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [:textarea.textarea
+         {:name :message
+          :value (:message @fields)
+          :on-change #(swap! fields
+                             assoc :message (-> % .-target .-value))}]]
+       [:input.button.is-primary
+        {:type :submit
+         :value "comment"}]
+       [:p "Name: " (:name @fields)]
+       [:p "Message: " (:message @fields)]])))
+
+(defn home []
+  [:div.content>div.columns.is-centered>div.column.is-two-thirds
+   [:div.columns>div.column
+    [message-form]]])
+
 (dom/render
- [:div
-  [:div {:id "hello", :class "content"} [:h1 "Hello, Auto!"]]
-  [:div#hello.content>h1 "Hello, Reagent"]]
+ [home]
  (.getElementById js/document "content"))
