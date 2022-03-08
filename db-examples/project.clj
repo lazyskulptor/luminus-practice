feea81fb4e020cc0a32fe624c1518807a620ecf8
Hyeonjun Park <lazyskulptor@gmail.com>
2022-03-08 23:58:41 +0900

    Appx4 -- Work with ... -- Setting Up .. -- Accessing the Database

--- db-examples/project.clj
+++ db-examples/project.clj
@@ -1,7 +1,5 @@
-(defproject db-examples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :repl-options {:init-ns db-examples.core})
+(defproject db-examples "0.1.0"
+  :description "examples of using next.jdbc"
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [seancorfield/next.jdbc "1.2.659"]
+                 [org.postgresql/postgresql "42.3.2"]])
--- db-examples/src/db_examples/core.clj
+++ db-examples/src/db_examples/core.clj
@@ -1,4 +1,8 @@
-(ns db-examples.core)
+(ns db-examples.core
+  (:require
+   [next.jdbc :as jdbc]
+   [next.jdbc.sql :as sql]
+   [next.jdbc.resut-set :as rs]))
 
 (defn foo
   "I don't do a whole lot."
