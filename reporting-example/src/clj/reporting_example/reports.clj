e9eafb49d73b623637b77b81b5d3c6a0a07c9c16
Hyeonjun Park <lazyskulptor@gmail.com>
2022-03-09 11:31:12 +0900

    Appx4 -- Generate Reports -- Displaying the Reports

--- reporting-example/src/clj/reporting_example/reports.clj
+++ reporting-example/src/clj/reporting_example/reports.clj
@@ -13,3 +13,25 @@
     [:chunk {:style :bold} "place: "] $palce "|n"
     [:chunk {:style :bold} "country: "] $country
     [:spacer]]))
+
+(defn table-report [out]
+  (let [data (db/read-employee)]
+    (pdf
+     [{:header "Employee List"}
+      (when-not (empty? data)
+        (into [:table
+               {:border false
+                :cell-border false
+                :header [{:backdrop-color [0 150 150]}
+                         "Name" "Occupation" "Place" "Country"]}]
+              (employee-template (db/read-employee))))]
+     out)))
+
+(defn list-report [out]
+  (pdf
+   [{}
+    [:heading {:size 10} "Employees"]
+    [:line]
+    [:spacer]
+    (employee-template-paragraph (db/read-employee))]
+   out))
